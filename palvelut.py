9624514828615d6938223ac89b55f944b921f14c
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-03-20 14:03:12 +0200

    Update palvelut.py
    getteri

--- palvelut.py
+++ palvelut.py
@@ -8,14 +8,24 @@ setter ja getter määritelmään jos halutaan suojatuksi jne.
 """
 
 class Palvelu:
-    def init(self, tuotenimi):
+    def __init__(self, tuotenimi):
         asiakkaat = []
 
 class ParempiPalvelu(Palvelu):
-    def init(self, tuotenimi):
+    def __init__(self, tuotenimi):
+        edut = []
 
 class Asiakas:
     """Luokka asettaa asiakkaalle numeron, nimen, iän ja luo numeron
     Julkiset methodit:
     """
-    def init(self, nimi, ika):
+    def __init__(self, nimi, ika):
+        
+    def luo_nro():
+
+    def set_arvo(self, nimi, ika):
+        if nimi and ika == False:
+            except ValueError:
+                print("")
+        
+    def get_arvo(self, nimi, ika, asiakasnumero):
